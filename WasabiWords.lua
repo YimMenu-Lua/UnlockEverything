83108461b1a8d10ac215131256b3ef123bf21e45
gir489returns <gir489returns@outlook.com>
2023-12-09 11:11:47 -0500

    Added Air-Freight trade price unlocks.

--- WasabiWords.lua
+++ WasabiWords.lua
@@ -531,6 +531,7 @@ wasabi_words:add_button("ShinyWasabi", function() --Original script by ShinyWasa
         stats.set_int("MPX_CONSECUTIVEDAYCOMPLETED", 7)
         stats.set_int("MPX_CONSECUTIVEWEEKCOMPLETED", 4)
         stats.set_int("MPX_AT_FLOW_VEHICLE_BS", -1) -- Trade price for dune4/dune5/wastelander/blazer5/phantom2/voltic2/technical2/boxville5/ruiner2
+        stats.set_int("MPX_LFETIME_HANGAR_BUY_COMPLET", 50) -- Trade price for microlight/rogue/alphaz1/havok/starling/molotok/tula/bombushka/howard/mogul/pyro/seabreeze/nokota/hunter
         for i = 0, 2 do --Unlock all daily rewards.
             local objective = globals.get_int(current_objectives_global + (1 + (0 * 5568)) + 681 + 4242 +  (1 + (i * 3)))
             globals.set_int(objectives_state_global + 1 + (1 + (i * 1)), objective)
